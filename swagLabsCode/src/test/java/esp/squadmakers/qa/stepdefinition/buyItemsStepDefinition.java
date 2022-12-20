616cc4f8a04c9e8c265c935403b827ae3d4264ca
Miguel Hugo <dragonmike6@gmail.com>
2022-12-20 13:52:02 -0600

    took more steps to implement

--- swagLabsCode/src/test/java/esp/squadmakers/qa/stepdefinition/buyItemsStepDefinition.java
+++ swagLabsCode/src/test/java/esp/squadmakers/qa/stepdefinition/buyItemsStepDefinition.java
@@ -2,13 +2,14 @@ package esp.squadmakers.qa.stepdefinition;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.en_scouse.An;
+import esp.squadmakers.qa.model.DataCheckout;
 import esp.squadmakers.qa.model.DataUserPass;
-import esp.squadmakers.qa.task.BuyItemsTask;
-import esp.squadmakers.qa.task.LoginUserPasswordTask;
-import esp.squadmakers.qa.task.OpenBrowser;
+import esp.squadmakers.qa.task.*;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actors.Cast;
 import net.serenitybdd.screenplay.actors.OnStage;
@@ -37,13 +38,29 @@ public class buyItemsStepDefinition {
 
     @When("^we login with username and password$")
     public void weLoginWithUsernameAndPassword(List<DataUserPass> data) {
-        int number = (int) (Math.random() * 3 + 1);
+        int number = (int) (Math.random() * 0 + 1);
         theActorInTheSpotlight().attemptsTo(LoginUserPasswordTask.withTheData(data.get(number)));
     }
 
-    @Then("^I want to buy one or more articles$")
-    public void iWantToBuyOneOrMoreArticles() {
-        theActorInTheSpotlight().attemptsTo(BuyItemsTask.buyItems());
+    @When("^I want to add to cart one or more articles$")
+    public void iWantToAddToCartOneOrMoreArticles() {
+        theActorInTheSpotlight().attemptsTo(AddItemToCart.buyItems());
+    }
+
+    @Then("^we go to the cart and buy the products$")
+    public void weGoToTheCartAndBuyTheProducts() {
+        theActorInTheSpotlight().attemptsTo(GoToShoppingCartTask.shoppingCartTask());
+    }
+
+    @Then("^fill in the requested data$")
+    public void fillInTheRequestedData(List<DataCheckout> dataCheckouts) {
+        int numberCheck = (int) (Math.random() * 2 + 1);
+        theActorInTheSpotlight().attemptsTo(CheckoutInformationTask.checkoutInformationTask(dataCheckouts.get(numberCheck)));
+    }
+
+    @And("^finish with the purchase$")
+    public void finishWithThePurchase() {
+        theActorInTheSpotlight().attemptsTo(FinishPurcharseTask.finishPurcharseTask());
     }
 
     @After
