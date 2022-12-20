9ea1507f06cdb5109f922019d989776d23ede9f4
Miguel Hugo <dragonmike6@gmail.com>
2022-12-20 13:51:16 -0600

    add other features to the gherkin language

--- swagLabsCode/src/main/java/esp/squadmakers/qa/task/AddItemToCart.java
+++ swagLabsCode/src/main/java/esp/squadmakers/qa/task/AddItemToCart.java
@@ -10,6 +10,7 @@ import net.serenitybdd.screenplay.actions.Scroll;
 import static esp.squadmakers.qa.userinterface.BuyItemsUserInterface.*;
 
 public class BuyItemsTask implements Task {
+
     @Override
     public <T extends Actor> void performAs(T actorCart) {
         actorCart.attemptsTo(Click.on(ARTICLE_ONE), WaitInteraction.waitFor(1));
@@ -18,7 +19,8 @@ public class BuyItemsTask implements Task {
         actorCart.attemptsTo(Click.on(ARTICLE_FOUR), WaitInteraction.waitFor(1),
                 Scroll.to(ARTICLE_FIVE));
         actorCart.attemptsTo(Click.on(ARTICLE_FIVE), WaitInteraction.waitFor(1));
-        actorCart.attemptsTo(Click.on(ARTICLE_SIX), WaitInteraction.waitFor(1));
+        actorCart.attemptsTo(Click.on(ARTICLE_SIX), WaitInteraction.waitFor(1),
+                Scroll.to(ARTICLE_ONE));
 
     }
 
--- swagLabsCode/src/test/resources/feature/buyItems.feature
+++ swagLabsCode/src/test/resources/feature/buyItems.feature
@@ -12,7 +12,12 @@ Feature: Buy Items
     When we login with username and password
       | username                | password     |
       | standard_user           | secret_sauce |
-      | locked_out_user         | secret_sauce |
-      | problem_user            | secret_sauce |
       | performance_glitch_user | secret_sauce |
-    Then I want to buy one or more articles
+    When I want to add to cart one or more articles
+    Then we go to the cart and buy the products
+    Then fill in the requested data
+      | firstName | lastName   | postalCode |
+      | Miguel    | Aguilar    | 93412      |
+      | Juan      | Jurado     | 94567      |
+      | Jorge     | Candelario | 92800      |
+    And finish with the purchase
