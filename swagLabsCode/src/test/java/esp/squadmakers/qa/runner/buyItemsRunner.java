07274537ac207fe7e4727d398b0a5c1612a270c9
Miguel Hugo <dragonmike6@gmail.com>
2022-12-20 11:07:39 -0600

    the executor was created to convert the gherkin language to java

---
+++ swagLabsCode/src/test/java/esp/squadmakers/qa/runner/buyItemsRunner.java
@@ -0,0 +1,13 @@
+package esp.squadmakers.qa.runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/resources/feature/buyItems.feature"
+        , glue = "esp.squadmakers.qa.stepdefinition"
+        , snippets = SnippetType.CAMELCASE)
+public class buyItemsRunner {
+}
